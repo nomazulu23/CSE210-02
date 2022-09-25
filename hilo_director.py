fb14dcbe7021d4fcfc9fc396c21c0e1354d41f6b
Heather Jasi <u033813021@byui.edu>
2022-09-25 17:34:37 +0200

    Updated commit

--- hilo_director.py
+++ hilo_director.py
@@ -8,13 +8,10 @@ class Hilo_Director():  # director of the game
         self.card = 0
         self.playing = True
         self.outcome = 300
+        self.guess = None
         # parameters assigned to the director of the game (object)
-
-        for i in range(1, 14):  # allows director to loop through series of numbers between 1 and 14
-            i = self.card
-            hilo = Hilo()  # variable for the imported Hilo class
-            # shows the card randomly picked between 1 to 13
-            print(f"Your card is: {self.card}")
+        self.hilo = Hilo()  # variable for the imported Hilo class
+        self.hilo.guess()
 
     def start_game(self):  # function to start playing the game and still under the Hilo_Director
 
@@ -26,34 +23,41 @@ class Hilo_Director():  # director of the game
 
     def user_guess(self):  # function to allow the user to guess
         # player guesses if the next card is higher or lower
-        guess = input("Guess if next card will be higher or lower: [h/l]")
-        if guess == 'h':
-            guess = self.playing
-            score.outcome += 100
-        elif guess == 'l':
-            guess = self.playing
+        print(f"The card number is {self.hilo.cardNum}")
+        self.guess = input("Guess if card picked is high or low: [h/l]")
+        if self.guess.lower() == "h":
+            self.playing = True
+        elif self.guess.lower() == "l":
+            self.playing = True
         else:
             self.playing = False
         # if the user correctly guesses high or low they continue playing
         # the result of the is stored as boolean value in the playing attribute
 
     def add_points(self):
+        
+        if self.playing:
+              # add 100points  or subtract 75 points
+            old_num = self.hilo.cardNum
+            self.hilo.guess()
+            if self.guess == "l" and old_num > self.hilo.cardNum:
+                self.outcome += 100
+            elif self.guess == "h" and old_num < self.hilo.cardNum:
+                self.outcome += 100
+            else:
+                self.outcome -= 75
 
-        while self.playing:
-            self.outcome += Hilo.points  # add 100points  or subtract 75 points
-           
 
         # displays of the card number picked
-        print(f"You picked {self.card}")
+        print(f"The card was {self.hilo.cardNum}")
         # shows the calculated total of points
         print(f" Your points are {self.outcome}")
 
     # if a player had more than 0 points they decide if they want to keep playing or not.
     def keep_playing(self):
-        while self.outcome > 0:
-            play = input("Do you want to keep playing: [yes/ no]")
-            if play == "yes":
-                play = self.playing
-            else:
-                play != self.playing
-                print("Game Over")
+        play = input("Do you want to keep playing: [yes/ no]")
+        if play == "yes":
+            self.playing = True
+        else:
+            self.playing = False
+            print("Game Over")
